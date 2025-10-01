b91dd95f283d8feb9c95542913d5acb286c7af85
Archiepena22 <penaarchie000@gmail.com>
2025-10-01 09:32:04 +0800

    master

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using StudentDiary_Services_PENA.Models;
 
 namespace StudentDiary_Services_PENA.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : Controller   
     {
         private readonly ILogger<HomeController> _logger;
 
